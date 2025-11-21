39578144f3c7c0a874eaeac9f8041da126eb52c3
Einarr07 <aidenperse@gmail.com>
2025-11-20 23:01:23 -0500

    Relations many to many

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -19,10 +19,19 @@ class Publisher(models.Model):
         return f'Name: {self.name} addres: {self.address}'
 
 
+class Author(models.Model):
+    name = models.TextField(max_length=200)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f'Name: {self.name} birth_date: {self.birth_date}'
+
+
 class Book(models.Model):
     title = models.TextField(max_length=2100)
     publication_date = models.DateField()
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)  # One to many
+    authors = models.ManyToManyField(Author, related_name='books')  # Many to many
 
     def __str__(self):
         return f'Title: {self.title} publication_date: {self.publication_date}'
